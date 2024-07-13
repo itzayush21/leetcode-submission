94f93b6abbee529ea7eb4a229e7789ee335edce2
Ayush Kumar singh <123325069+itzayush21@users.noreply.github.com>
2024-07-13 23:51:01 +0530

    Time: 113 ms (16.22%), Space: 12.4 MB (57.93%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        def is_palindrome(left, right):
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left, right = left + 1, right - 1
+            return True
+
+        left, right = 0, len(s) - 1 
+
+        
+        while left < right:
+            
+            if s[left] != s[right]:
+                return is_palindrome(left, right - 1) or is_palindrome(left + 1, right)
+            left, right = left + 1, right - 1
+
+        return True
