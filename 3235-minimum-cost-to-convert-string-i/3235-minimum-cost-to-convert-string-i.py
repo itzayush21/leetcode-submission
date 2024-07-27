8a9e3bb96369e7368fdbbbbfc540459b650c1be3
Ayush Kumar singh <123325069+itzayush21@users.noreply.github.com>
2024-07-27 17:41:03 +0530

    Time: 2163 ms (75%), Space: 23.2 MB (25%) - LeetHub

---
+++ 3235-minimum-cost-to-convert-string-i/3235-minimum-cost-to-convert-string-i.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def minimumCost(self, source, target, original, changed, cost):
+        """
+        :type source: str
+        :type target: str
+        :type original: List[str]
+        :type changed: List[str]
+        :type cost: List[int]
+        :rtype: int
+        """
+        inf=10**20
+        g = [[inf] * 26 for _ in range(26)]
+        for x, y, z in zip(original, changed, cost):
+            x = ord(x) - ord('a')
+            y = ord(y) - ord('a')
+            g[x][y] = min(g[x][y], z)
+        for k in range(26):
+            for i in range(26):
+                for j in range(26):
+                    g[i][j] = min(g[i][j], g[i][k] + g[k][j])
+        ans = 0
+        for a, b in zip(source, target):
+            if a != b:
+                x, y = ord(a) - ord('a'), ord(b) - ord('a')
+                if g[x][y] >=inf:
+                    return -1
+                ans += g[x][y]
+        return ans
+        
