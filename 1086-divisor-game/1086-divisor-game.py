d3bf52f12b2aa5b3b46e632c68aa901d48f09cf9
Ayush Kumar singh <123325069+itzayush21@users.noreply.github.com>
2024-10-18 19:07:46 +0530

    Time: 3 ms (100%), Space: 16.8 MB (5.72%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.py
+++ 1086-divisor-game/1086-divisor-game.py
@@ -1,3 +1,9 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        return n%2==0
+        if n<=1:
+            return False
+        for i in range(1,n):
+            if n%i==0:
+                return not Solution.divisorGame(self,n-i)
+        return False
+        
