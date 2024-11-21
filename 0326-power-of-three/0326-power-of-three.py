483b70e073888c7c9198fe06683d85175b0a013e
Ayush Kumar singh <123325069+itzayush21@users.noreply.github.com>
2024-11-21 18:42:23 +0530

    Time: 19 ms (28.14%), Space: 17.1 MB (11.27%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n==0 or n<0:
+            return False
+        x=math.log10(n)/math.log10(3)
+        print(x)
+        return ceil(x)==floor(x)
