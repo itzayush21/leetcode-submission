d204d587ce32a32cb4377877b316b2ad72aec0a0
Ayush Kumar singh <123325069+itzayush21@users.noreply.github.com>
2025-01-13 13:38:00 +0530

    Time: 66 ms (9.42%), Space: 18.7 MB (5.83%) - LeetHub

---
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.py
@@ -0,0 +1,61 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class NestedIterator(object):
+
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        """
+        self.stack=[]
+        self.dfs(nestedList)
+        
+        self.stack.reverse()
+        
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        return self.stack.pop()
+        
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return len(self.stack)>0
+    def dfs(self,nested):
+        for n in nested:
+            if n.isInteger():
+                self.stack.append(n)
+            else:
+                self.dfs(n.getList())
+        
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
