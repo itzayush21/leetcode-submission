e95ae49db9f45ac74914e787bc20ce869c2947c0
Ayush Kumar singh <123325069+itzayush21@users.noreply.github.com>
2026-09-17 16:13:46 +0530

    Time: 1918 ms (6.21%), Space: 14.5 MB (17.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,10 +1,62 @@
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        dp=[amount+1]*(amount+1)
-        dp[0]=0
-        for a in range(1,amount+1):
-            for c in coins:
-                if a-c>=0:
-                    dp[a]=min(dp[a],1+dp[a-c])
-        print(dp)
-        return dp[amount] if dp[amount]!=amount+1 else -1
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+
+        dp = [[float('inf')] * (amount + 1) for _ in range(len(coins) + 1)]
+
+        
+        for i in range(len(coins) + 1):
+            dp[i][0] = 0
+
+
+        for i in range(1,len(coins)+1):
+            for j in range(1,amount+1):
+
+                not_take=dp[i-1][j]
+
+                take=float('inf')
+                if coins[i-1]<=j:
+                    take=1+dp[i][j-coins[i-1]]
+
+                dp[i][j]=min(not_take,take)
+
+        ans=dp[len(coins)][amount]
+
+        if ans == float('inf'):
+            return -1
+
+        return ans
+
+
+        # def helper(i,amt):
+
+        #     if i==len(coins):
+        #         return float('inf')
+
+        #     if amt==0:
+        #         return 0
+
+        #     if (i,amt) in dp:
+        #         return dp[(i,amt)]
+
+        #     not_take=helper(i+1,amt)
+            
+        #     take=float('inf')
+        #     if coins[i]<=amt:
+        #         take=1+helper(i,amt-coins[i])
+
+        #     dp[(i,amt)]=min(take,not_take)
+
+        #     return dp[(i,amt)]
+
+        # ans = helper(0, amount)
+
+        # if ans == float('inf'):
+        #     return -1
+
+        # return ans
+        
